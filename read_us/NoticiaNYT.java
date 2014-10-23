d46cf230f66ccf50064b151a1d9d341706e9af7e
Eduard R. Ockerse <eduardockerse@gmail.com>
2014-10-24 00:22:02 +0200

    cosas nuevas y editadas
    menudo lio de cosas con gson json con archivos no tan simples como el
    ejemplo de clase de Facebook..

---
+++ read_us/NoticiaNYT.java
@@ -0,0 +1,13 @@
+class NoticiaNYT {
+	String web_url;
+	String main; //NOMBRE RESERVADO???
+	String lead_paragraph;
+	String pub_date;
+
+	public NoticiaNYT (String web_url, String main, String lead_paragraph, String pub_date){
+		this.web_url = web_url;
+		this.main = main;
+		this.lead_paragraph = lead_paragraph;
+		this.pub_date= pub_date;ç
+	}
+}
