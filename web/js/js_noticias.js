3ac71388e4eade10b29b7f4c492207e57da2c253
Eduard R. Ockerse <eduardockerse@gmail.com>
2014-10-23 21:22:07 +0200

    js_noticias

--- web/js/js_noticias.js
+++ web/js/js_noticias.js
@@ -4,22 +4,22 @@
  * and open the template in the editor.
  */
 function insertar_noticia() {
-    var res = "HOLAAAA";    
-//    var res= "<div class=\"col-lg-12 text-center\">";
-//    res+= "<img class=\"img-responsive img-border img-full\" src=\"img/slide-2.jpg\" alt=\"\">";
-//    res+= "<h2>Reach for the sky!";
-//    res+= "<br>";
-//    res+= "<small>September 24th, 2014</small>";
-//    res+= "</h2>";
-//    res+= "<p>It is almost 75 years since they enjoyed their finest hour in the Battle of Britain.";
-//    res+= "And now, an Oxfordshire flying club has marked the momentous occasion by creating a classic Spitfire, albeit in a slightly smaller form than the World War Two version.</p>";
-//    res+= "<a href=\"http://www.warhistoryonline.com/war-articles/reach-sky-flying-instructor-wants-build-12-new-spitfires-mark-75th-anniversary-battle-britain.html\" class=\"btn btn-default btn-lg\">Read full article</a>";
-//    res+= "<hr>";
-//    res+= "</div>";
+ 
+    var res= "<div class=\"col-lg-12 text-center\">";
+    res+= "<img class=\"img-responsive img-border img-full\" src=\"img/slide-2.jpg\" alt=\"\">";
+    res+= "<h2>Reach for the sky!";
+    res+= "<br>";
+    res+= "<small>September 24th, 2014</small>";
+    res+= "</h2>";
+    res+= "<p>It is almost 75 years since they enjoyed their finest hour in the Battle of Britain.";
+    res+= "And now, an Oxfordshire flying club has marked the momentous occasion by creating a classic Spitfire, albeit in a slightly smaller form than the World War Two version.</p>";
+    res+= "<a href=\"http://www.warhistoryonline.com/war-articles/reach-sky-flying-instructor-wants-build-12-new-spitfires-mark-75th-anniversary-battle-britain.html\" class=\"btn btn-default btn-lg\">Read full article</a>";
+    res+= "<hr>";
+    res+= "</div>";
     
-    //return res;
+    return res;
     
-    return "MEH";
+    
 }
 
 document.write(insertar_noticia());
