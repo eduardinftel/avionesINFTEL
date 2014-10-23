836c12b63d1b395df11fbe7e4ae4d01816d5be35
Eduard R. Ockerse <eduardockerse@gmail.com>
2014-10-24 00:42:15 +0200

    simplificando archivo json

--- read_us/NoticiaNYT.java
+++ read_us/NoticiaNYT.java
@@ -1,13 +1,13 @@
 class NoticiaNYT {
 	String web_url;
-	String main; //NOMBRE RESERVADO???
+	String title;
 	String lead_paragraph;
 	String pub_date;
 
-	public NoticiaNYT (String web_url, String main, String lead_paragraph, String pub_date){
+	public NoticiaNYT (String web_url, String title, String lead_paragraph, String pub_date){
 		this.web_url = web_url;
-		this.main = main;
+		this.title = title;
 		this.lead_paragraph = lead_paragraph;
-		this.pub_date= pub_date;ç
+		this.pub_date= pub_date;
 	}
 }
