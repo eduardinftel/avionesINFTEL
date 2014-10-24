decd7595a349e5957e3215c8f4d4ab9ad8244060
eduardinftel <eduardinftel@gmail.com>
2014-10-24 16:39:28 +0200

    cambios en noticias.js

--- web/js/noticias.js
+++ web/js/noticias.js
@@ -23,15 +23,15 @@ function cargarNoticia(titulo, texto, fecha, url) {
 
 }
 
+
+var arrayNoticias;
+var i;
 $(document).ready(function () {
-    var arrayNoticias;
     $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Supermarine+Spitfire&api-key=b1e8bdbc3fa9841be66f9cb3bab690b7%3A4%3A70045411", function (result) {
         arrayNoticias = result.response.docs;
+        console.log(result.response.docs);
     });
-    var i;
-    console.log(arrayNoticias);
     for (i in arrayNoticias) {
         cargarNoticia(arrayNoticias[i].headline.main, arrayNoticias[i].lead_paragraph, arrayNoticias[i].pub_date, arrayNoticias[i].web_url);
-        console.log(arrayNoticias[i].headline.main);
     }
 });
