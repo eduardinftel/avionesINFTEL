ffb5efd3f42634d38e8f0576d27aa78a9176f56f
eduardinftel <eduardinftel@gmail.com>
2014-10-24 12:09:33 +0200

    mejorando y adelantando trabajo!

---
+++ web/js/noticias.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function nuevoContenido()
+{
+    document.getElementById("idNoticias").innerHTML = ("este es el nuevo contenido cambiado");
+
+}
+        
